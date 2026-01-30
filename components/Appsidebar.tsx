df6b016c564b798a0b7415de8df9363d582d6701
Aryan Nandanwar <aryannandanwar738@gmail.com>
2026-01-30 22:19:06 +0530

    collapsable Navbar added

--- components/Appsidebar.tsx
+++ components/Appsidebar.tsx
@@ -1,8 +1,109 @@
 import React from 'react'
+import { Home, Inbox, Calendar, Search, Settings, User2, ChevronUp } from 'lucide-react'
+import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader,SidebarFooter, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from './ui/sidebar'
+import Link from 'next/link'
+import Image from 'next/image'
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
+import { Separator } from './ui/separator'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from './ui/dropdown-menu'
+
+const items = [
+  {
+    title: "Home",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Inbox",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "Calendar",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Search",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+]
 
 const Appsidebar = () => {
   return (
-    <div>Appsidebar</div>
+    <div>
+      
+      <Sidebar collapsible='icon'>
+
+<SidebarHeader>
+<SidebarMenu>
+  <SidebarMenuItem>
+    <SidebarMenuButton asChild>
+      
+    <Link href="/">
+    <Avatar>
+  <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn"  />
+    <AvatarFallback>CN</AvatarFallback>
+
+  </Avatar>
+    <span>Aryan Nandanwar</span>
+    </Link>
+
+    </SidebarMenuButton>
+  </SidebarMenuItem>
+</SidebarMenu>
+
+</SidebarHeader>
+
+<Separator />
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+
+<SidebarFooter>
+  <SidebarMenu>
+    <SidebarMenuItem>
+      <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+      <SidebarMenuButton >
+        <User2 /> Aryan <ChevronUp className='ml-auto' />
+      </SidebarMenuButton>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align='end'>
+<DropdownMenuItem>Account</DropdownMenuItem>
+<DropdownMenuItem>Settings</DropdownMenuItem>
+<DropdownMenuItem>Sign out</DropdownMenuItem>
+      </DropdownMenuContent>
+      </DropdownMenu>
+    </SidebarMenuItem>
+  </SidebarMenu>
+</SidebarFooter>
+
+    </Sidebar>
+
+    </div>
   )
 }
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,22 +1,51 @@
+"use client"
+
 import React from 'react'
 import Link from 'next/link'
-import { Moon, User,Settings, LogOut } from 'lucide-react'
+import { Moon,Sun, User,Settings, LogOut } from 'lucide-react'
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu'
+import { useTheme } from "next-themes"
+import { Button } from './ui/button'
+import { SidebarTrigger } from './ui/sidebar'
+
 
 const Navbar = () => {
+  const { setTheme } = useTheme();
   return (
     <nav className='p-4 flex justify-between items-center'>
 {/* LEFT */}
-    collapseButton
+    <SidebarTrigger />
 
     {/* RIGHT */}
 <div className='flex items-center gap-4'>   
     <Link href="/">Dashbord</Link>
-    <Moon />
+    {/* Mode */}
 
+      <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+
+{/* Profile */}
     <DropdownMenu>
-  <DropdownMenuTrigger>
+  <DropdownMenuTrigger asChild>
    <Avatar>
   <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
   <AvatarFallback>CN</AvatarFallback>
@@ -36,7 +65,7 @@ const Navbar = () => {
       </DropdownMenuItem>
     </DropdownMenuGroup>
     <DropdownMenuGroup>
-      <DropdownMenuItem>
+      <DropdownMenuItem variant='destructive'>
         <LogOut className='h-[1.2rem] w-[1.2 rem] mr-2'/>Log out
       </DropdownMenuItem>
     </DropdownMenuGroup>
